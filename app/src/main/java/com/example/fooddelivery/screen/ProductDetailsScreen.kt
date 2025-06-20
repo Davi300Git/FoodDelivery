268cedd8ea8545e072b9f263f185fd759c2498c1
Davi Sandro dos Santos <93402520+Davi300Git@users.noreply.github.com>
2025-06-20 19:42:57 -0300

    Product Description
    Product description section completed

--- app/src/main/java/com/example/fooddelivery/screen/ProductDetailsScreen.kt
+++ app/src/main/java/com/example/fooddelivery/screen/ProductDetailsScreen.kt
@@ -3,12 +3,14 @@ package com.example.fooddelivery.screen
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.example.fooddelivery.data.ProductDescriptionData
 import com.example.fooddelivery.data.ProductFlavorData
 import com.example.fooddelivery.data.ProductFlavorState
 import com.example.fooddelivery.data.ProductHighLighState
@@ -16,6 +18,7 @@ import com.example.fooddelivery.data.ProductNutritionData
 import com.example.fooddelivery.data.ProductNutritionState
 import com.example.fooddelivery.data.ProductPreviewState
 import com.example.fooddelivery.screen.components.FlavorSection
+import com.example.fooddelivery.screen.components.ProductDescriptionSection
 import com.example.fooddelivery.screen.components.ProductNutritionSection
 import com.example.fooddelivery.screen.components.ProductPreviewSection
 
@@ -24,8 +27,8 @@ fun ProductDetailsScreen(
     modifier: Modifier = Modifier,
     productPreviewState: ProductPreviewState = ProductPreviewState(),
     productFlavors: List<ProductFlavorState> = ProductFlavorData,
-    productNutritionState: ProductNutritionState = ProductNutritionData
-
+    productNutritionState: ProductNutritionState = ProductNutritionData,
+    productDescripton: String = ProductDescriptionData
 ) {
     val scrollableState = rememberScrollState()
 
@@ -48,6 +51,16 @@ fun ProductDetailsScreen(
         ProductNutritionSection(
             modifier = Modifier.padding(horizontal = 18.dp),
             state = productNutritionState
+        )
+        Spacer(
+            modifier = Modifier.height(32.dp)
+        )
+        ProductDescriptionSection(
+            productDescription = productDescripton,
+            modifier = Modifier
+                .navigationBarsPadding()
+                .padding(horizontal = 18.dp)
+                .padding(bottom = 24.dp)
         )
     }
 }
